2c7d1c0839e5bec7a126c3a8048b38fe639d6b2b
VolroDev <volrodev@gmail.com>
2023-09-14 12:56:57 +0300

    доробки на ноуті

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ sky_surface = pygame.image.load('graphics/Screenshot_3.jpg')
 ground_surface = pygame.image.load('graphics/Screenshot_4.jpg')
 test_surface = test_front.render('My game',False, 'Green')
 
+snail_surface = pygame.image.load('graphics/Screenshot_2.jpg')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,6 +29,9 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(test_surface,(300,50))
-
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,200))
+    
     pygame.display.update()
     clock.tick(60)
