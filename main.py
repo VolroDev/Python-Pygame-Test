cfe14d5242be084d0e1f0cce3d37e17f6ce006c9
VolroDev <104170926+VolroDev@users.noreply.github.com>
2023-10-15 13:06:16 +0300

    Поміщення оновлень до 15.10.2023

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+#Підключення модулів
 import pygame
 from sys import exit
 
+#Ініціалізація бібліотеки
 pygame.init()
+
+#Вказання розміру вікна
 screen = pygame.display.set_mode((800,400))
+
+#Вказаня назви вікна
 pygame.display.set_caption('Runner')
+
+#Ініцілазація часу
 clock = pygame.time.Clock()
+
+#Ініціалазіація тексту
 test_front = pygame.font.Font('font/Pixeltype.ttf', 50)
+test_surface = test_front.render('My game',False, 'Green')
+#Ініціалазіація фонів
+sky_surface = pygame.image.load('graphics/Screenshot_3.jpg').convert()
+ground_surface = pygame.image.load('graphics/Screenshot_4.jpg').convert()
+
+#Ініціалазіація обєктів 
+snail_surf = pygame.image.load('graphics/Screenshot_2_2.jpg').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+snail_x_pos = 600
+player_surf = pygame.image.load('graphics/Screenshot_1.jpg').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (100,300))
 
 '''
 Приклад червоного куба
@@ -13,25 +34,30 @@ test_surface = pygame.Surface((100,200))
 test_surface.fill('Red')
 '''
 
-sky_surface = pygame.image.load('graphics/Screenshot_3.jpg')
-ground_surface = pygame.image.load('graphics/Screenshot_4.jpg')
-test_surface = test_front.render('My game',False, 'Green')
-
-snail_surface = pygame.image.load('graphics/Screenshot_2.jpg')
-snail_x_pos = 600
-
+#Початок програми
 while True:
+    #Перевірка евентів
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
+    #Відображення обєектів
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(test_surface,(300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,200))
     
+    snail_rect.x -= 4
+    if snail_rect.right <=0: 
+        snail_rect.left = 800
+    
+    screen.blit(player_surf,player_rect)
+    screen.blit(snail_surf,snail_rect)
+
+    print(player_rect.colliderect(snail_rect))
+
+    #Оновлення відображення
     pygame.display.update()
+    #Кількість кадрів в секунду
     clock.tick(60)
